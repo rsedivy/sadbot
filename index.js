e5c172a4309955ed8e69d253c27a0447d01bfc85
Richard <rsedivy@gmail.com>
2020-07-28 10:43:47 +0200

    read blacklist file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 
 const Discord = require('discord.js');
+const fs = require('fs');
 let config = null;
 
 try{
@@ -17,13 +18,45 @@ try{
 
 const client = new Discord.Client();
 
+let userBlacklist = [];
+let channelBlacklist = [];
+
 client.once('ready', () => {
     console.log('Ready!');
+
+    if(!fs.existsSync("./userGifBlacklist")){
+        fs.writeFileSync("./userGifBlacklist", null);
+    }
+    if(!fs.existsSync("./channelGifBlacklist")){
+        fs.writeFileSync("./channelGifBlacklist", null);
+    }
+
+    fs.readFile("./userGifBlacklist", 'utf8', (err,data) => {
+        if(!err){
+            userBlacklist = data.split(/\n+/).map(x => x.split("|").map(y => y.trim()));
+        }else{
+            console.error("Unable to read user blacklist file. Error log below\n" +
+                "----------------------------\n" + err);
+            process.exit(1);
+        }
+    });
+
+    fs.readFile("./channelGifBlacklist", 'utf8', (err,data) => {
+        if(!err){
+            channelBlacklist = data.split(/\n+/).map(x => x.split("|").map(y => y.trim()));
+        }else{
+            console.error("Unable to read channel blacklist file. Error log below\n" +
+                "----------------------------\n" + err);
+            process.exit(1);
+        }
+    });
+
 });
 
 client.login(config.token);
 
 client.on('message', message => {
+    console.log(message.content);
     if(message.content.startsWith("?sadbot")){
         const args = message.content.slice(1).trim().split(/ +/);
         const command = args.shift().toLowerCase();
@@ -33,9 +66,13 @@ client.on('message', message => {
                 help(message.channel);
                 break;
             case 'nogif':
-                if(args[1]){
+                if(args[2]){
                     //console.log(args[1])
-                }else{
+                }
+                else if(args[1]){
+
+                }
+                else{
                     // yes this is hardcoded to nogif Happy
                     message.channel.send("No user specified. Muting <@!684410145496891403>");
                 }
@@ -53,7 +90,7 @@ function help(channel){
         "SadBot v1.0 command list:\n" +
         "?sadbot | ?sadbot about         : Display bot info\n" +
         "?sadbot help                    : Show this help message\n" +
-        "?sadbot nogif                   : Stops Happy from posting gifs for 10 minutes (hardcoded)\n" +
+        "?sadbot nogif                   : Stops Happy/Hannerz from posting gifs for 10 minutes (hardcoded)\n" +
         "?sadbot nogif <@user>           : Stops a user from posting gifs for 10 minutes\n" +
         "?sadbot nogif <@user> <min>     : Stops a user from posting gifs for <min> minutes\n" +
         "?sadbot nogif <@user> perma     : Stops a user from posting gifs permanently\n"+
